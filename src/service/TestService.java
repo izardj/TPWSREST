553478cf72045c29bffc7eeb9d783ae4c0dc0f51
izardj <izard.jerome@free.fr>
2017-05-05 11:31:24 +0200

    Exposition d'autre methodes en webservice REST

--- src/service/TestService.java
+++ src/service/TestService.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -14,21 +15,27 @@ public class TestService {
 	@GET
 	@Path("/test")
 	@Produces(MediaType.TEXT_PLAIN)
-	public String test(){
+	public String test() {
 		return "TEST";
 	}
-	
-	public double conversion(double mt){
-		return 5*mt;
+
+	@GET
+	@Path("/conversion/{montant}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public double conversion(@PathParam("montant") double mt) {
+		return 5 * mt;
 	}
-	
-	public List<String> getInfos(){
+
+	@GET
+	@Path("/infos")
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<String> getInfos() {
 		List<String> liste = new ArrayList<String>();
 		liste.add("A");
 		liste.add("B");
 		liste.add("C");
 		liste.add("D");
-		
+
 		return liste;
 	}
 }
